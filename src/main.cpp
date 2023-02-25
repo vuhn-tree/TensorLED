4533df2aaa33c5475f05ae8616e1e7db2420fa10
Richard Vu <rqvu.mail@gmail.com>
2023-02-25 14:04:57 -0800

    RGB test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
-#include <Arduino.h>
+#include <M5Core2.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 32       // Port A
+#define NUMPIXELS 3  
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(
+    NUMPIXELS, PIN,
+    NEO_GRB +
+        NEO_KHZ800);  //set number of LEDs, pin number, LED type.
 
 void setup() {
-  // put your setup code here, to run once:
+  M5.begin();      
+  pixels.begin();  
+  M5.Lcd.println(("RGB Tester"));
 }
 
+int i = 0, j = 1, k = 2;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  pixels.setPixelColor(i++, pixels.Color(100, 0, 0));  // Bright red
+  pixels.setPixelColor(j++, pixels.Color(0, 100, 0));  // Bright green
+  pixels.setPixelColor(k++, pixels.Color(0, 0, 100));  // Bright blue
+  pixels.show();  //sends the updated color to the hardware.
+  delay(500);
+  if (i == 3)
+    i = 0;
+  else if (j == 3)
+    j = 0;
+  else if (k == 3)
+    k = 0;
 }
