a659e9da6d191934395fccf39280f7e93bc4e50c
Richard Vu <rqvu.mail@gmail.com>
2023-02-26 00:55:28 -0800

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,22 @@
-#include <M5Core2.h>
 #include <Adafruit_NeoPixel.h>
+#include <M5Core2.h>
 
-#define PIN 32       // Port A
-#define NUMPIXELS 3  
+#define PIN 32  // Port A
+#define NUMPIXELS 3
 
 const int INFO_HEIGHT_POS = 25;
 
-
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(
     NUMPIXELS, PIN,
-    NEO_GRB +
-        NEO_KHZ800);  //set number of LEDs, pin number, LED type.
+    NEO_GRB + NEO_KHZ800);  // set number of LEDs, pin number, LED type.
 
 uint32_t RGB_RED = pixels.Color(100, 0, 0);
 uint32_t RGB_GREEN = pixels.Color(0, 100, 0);
 uint32_t RGB_BLUE = pixels.Color(0, 0, 100);
 
 void setup() {
-  M5.begin();      
-  pixels.begin();  
+  M5.begin();
+  pixels.begin();
   pixels.setBrightness(10);
 
   M5.Axp.SetLcdVoltage(2600);
@@ -37,35 +35,35 @@ void loop() {
   sprintf(buf, "Powr Temp: %2.1fC", powerTemp);
   M5.Lcd.drawString(buf, 0, INFO_HEIGHT_POS, 4);
 
-  const float batV =  M5.Axp.GetBatVoltage();
-  const float batA =  M5.Axp.GetBatCurrent();
+  const float batV = M5.Axp.GetBatVoltage();
+  const float batA = M5.Axp.GetBatCurrent();
   sprintf(buf, "Batt.: %2.2fV %2.1fmA", batV, batA);
   M5.Lcd.drawString(buf, 0, INFO_HEIGHT_POS + 25, 4);
 
-  const float busV =  M5.Axp.GetVBusVoltage();
-  const float busA =  M5.Axp.GetVBusCurrent();
+  const float busV = M5.Axp.GetVBusVoltage();
+  const float busA = M5.Axp.GetVBusCurrent();
   sprintf(buf, "VBUS: %2.2fV %2.1fmA", busV, busA);
   M5.Lcd.drawString(buf, 0, INFO_HEIGHT_POS + 50, 4);
 
-  const float vinV =  M5.Axp.GetVinVoltage();
-  const float vinA =  M5.Axp.GetVinCurrent();
+  const float vinV = M5.Axp.GetVinVoltage();
+  const float vinA = M5.Axp.GetVinCurrent();
   sprintf(buf, "vin: %2.2fV %2.1fmA", vinV, vinA);
   M5.Lcd.drawString(buf, 0, INFO_HEIGHT_POS + 75, 4);
 
-  if(M5.BtnB.read()) {
+  if (M5.BtnB.read()) {
     pixels.setPixelColor(1, RGB_GREEN);
   } else {
     pixels.setPixelColor(1, RGB_RED);
   }
 
-  pixels.show();  //sends the updated color to the hardware.
+  pixels.show();  // sends the updated color to the hardware.
   delay(500);
 
-  if(M5.BtnA.read()) {
-    M5.shutdown();  
-  } 
+  if (M5.BtnA.read()) {
+    M5.shutdown();
+  }
 
-  if(M5.BtnC.read()) {  
+  if (M5.BtnC.read()) {
     M5.Axp.SetLcdVoltage(3300);
   }
 }
