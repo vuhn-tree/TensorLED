1e29618ba4dc90c75ee0507f4dfbfc4182c75475
Richard Vu <rqvu.mail@gmail.com>
2023-03-02 01:00:38 -0800

    removed unused variables

--- serpy.py
+++ serpy.py
@@ -4,11 +4,6 @@ ser = serial.Serial()
 ser.baudrate = 115200
 ser.port = '/dev/cu.wchusbserial51850172361'
 ser.open()
-
-values = bytearray([4, 9, 62, 144, 56, 30, 147, 3,
-                    210, 89, 111, 78, 184, 151, 17, 129])
-
-# ser.write(values)
 ser.write(sys.argv[1])
 
 total = 0
