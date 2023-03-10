8fce449d3ee580f52279f3ffa4a81ede3c349809
Richard Vu <rqvu.mail@gmail.com>
2023-03-10 01:00:47 -0800

    defined models

--- ledCamature.py
+++ ledCamature.py
@@ -5,3 +5,10 @@ MODELS_DIR = os.path.join(DATA_DIR, 'models')
 for dir in [DATA_DIR, MODELS_DIR]:
     if not os.path.exists(dir):
         os.mkdir(dir)
+
+import tarfile
+import urllib.request
+
+# Download and extract model
+MODEL_DATE = '20200711'
+MODEL_NAME = 'ssd_resnet101_v1_fpn_640x640_coco17_tpu-8'
