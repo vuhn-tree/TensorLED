468fdddf409d92057f22d9e63deff250c68492e0
Richard Vu <rqvu.mail@gmail.com>
2023-03-13 20:48:48 -0700

    save changes

--- ledCamature.py
+++ ledCamature.py
@@ -1,5 +1,26 @@
 #import os, cv2, glob
 import cv2
+ 
+# Path to image in local directory
+path = '/Users/vuhn/Desktop/test.png'
+ 
+# Using cv2.imread() to read an image in grayscale mode
+image = cv2.imread(path, 0)
+ 
+# Using namedWindow()
+# A window with 'Display_Image' name is created
+# with WINDOW_NORMAL allowing us to have random size
+cv2.namedWindow("Display_Image", cv2.WINDOW_NORMAL)
+ 
+# Using cv2.imshow() to display the image
+cv2.imshow('Display_Image', image)
+ 
+# Waiting 0ms for user to press any key
+cv2.waitKey(1000)
+ 
+# Using cv2.destroyAllWindows() to destroy
+# all created windows open on screen
+cv2.destroyAllWindows()
 
 # DATA_DIR = os.path.join(os.getcwd(), 'data')
 # MODELS_DIR = os.path.join(DATA_DIR, 'models')
@@ -31,8 +52,8 @@ import cv2
 #     cap.release()
 
 # cap = cv2.VideoCapture(camera)
-cv2.startWindowThread()
-cv2.namedWindow('displaymywindows', cv2.WINDOW_AUTOSIZE)
+# cv2.startWindowThread()
+# cv2.namedWindow('displaymywindows', cv2.WINDOW_AUTOSIZE)
 
 # cap = cv2.VideoCapture(2)
 
