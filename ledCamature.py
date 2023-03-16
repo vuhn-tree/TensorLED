0939ff44b5033611d1fbd182719ff06cabc07420
Richard Vu <rqvu.mail@gmail.com>
2023-03-16 03:40:27 -0700

    cleanup

--- ledCamature.py
+++ ledCamature.py
@@ -1,26 +1,5 @@
 #import os, cv2, glob
 import cv2
- 
-# Path to image in local directory
-path = '/Users/vuhn/Desktop/test.png'
- 
-# Using cv2.imread() to read an image in grayscale mode
-image = cv2.imread(path, 0)
- 
-# Using namedWindow()
-# A window with 'Display_Image' name is created
-# with WINDOW_NORMAL allowing us to have random size
-cv2.namedWindow("Display_Image", cv2.WINDOW_NORMAL)
- 
-# Using cv2.imshow() to display the image
-cv2.imshow('Display_Image', image)
- 
-# Waiting 0ms for user to press any key
-cv2.waitKey(1000)
- 
-# Using cv2.destroyAllWindows() to destroy
-# all created windows open on screen
-cv2.destroyAllWindows()
 
 # DATA_DIR = os.path.join(os.getcwd(), 'data')
 # MODELS_DIR = os.path.join(DATA_DIR, 'models')
@@ -35,40 +14,20 @@ cv2.destroyAllWindows()
 # MODEL_DATE = '20200711'
 # MODEL_NAME = 'ssd_resnet101_v1_fpn_640x640_coco17_tpu-8'
 
-# for camera in glob.glob("/dev/video?"):
-#     cap = cv2.VideoCapture(camera)
-#     if not cap.isOpened():
-#         raise IOError("Cannot open webcam")
+cap = cv2.VideoCapture(0)
 
-#     while True:
-#         ret, frame = cap.read()
-#         frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-#         cv2.imshow('Input', frame)
+#Check if the webcam is opened correctly
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
 
-#         c = cv2.waitKey(1)
-#         if c == 27:
-#             break
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    cv2.imshow('Input', frame)
 
-#     cap.release()
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
 
-# cap = cv2.VideoCapture(camera)
-# cv2.startWindowThread()
-# cv2.namedWindow('displaymywindows', cv2.WINDOW_AUTOSIZE)
-
-# cap = cv2.VideoCapture(2)
-
-# #Check if the webcam is opened correctly
-# if not cap.isOpened():
-#     raise IOError("Cannot open webcam")
-
-# while True:
-#     ret, frame = cap.read()
-#     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-#     cv2.imshow('Input', frame)
-
-#     c = cv2.waitKey(1)
-#     if c == 27:
-#         break
-
-# cap.release()
-# cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
