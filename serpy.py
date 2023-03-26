3b6f1d913a79c53f4fe4d1e90c79606cbc0e20a4
Richard Vu <rqvu.mail@gmail.com>
2023-03-25 21:51:37 -0700

    added parsing

--- serpy.py
+++ serpy.py
@@ -1,5 +1,5 @@
 import serial
-import sys
+import sys, argparse
 
 ser = serial.Serial()
 ser.baudrate = 9600
@@ -8,7 +8,13 @@ ser.port = '/dev/cu.wchusbserial51851974134'
 ser.open()
 print("Current port: " + ser.port)
 
+parser = argparse.ArgumentParser()
+parser.add_argument(color='color',)
 message = sys.argv[1].encode()
+
+if (message == 'm')
+
+
 print("Writing command: " + message.decode())
 ser.write(message)
 
