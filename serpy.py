9f438e0a800397ba46dae46a274d8cc7bfcd4c52
Richard Vu <rqvu.mail@gmail.com>
2023-03-26 01:10:58 -0700

    help options

--- serpy.py
+++ serpy.py
@@ -4,12 +4,11 @@ import sys, argparse
 ser = serial.Serial()
 ser.baudrate = 9600
 ser.port = '/dev/cu.wchusbserial51850172361'
-#ser.port = '/dev/cu.wchusbserial51851974134'
 ser.open()
 print("Current port: " + ser.port)
 
 parser = argparse.ArgumentParser()
-parser.add_argument(dest='color',)
+parser.add_argument("color", help="b, g, r")
 
 args = parser.parse_args()
 
