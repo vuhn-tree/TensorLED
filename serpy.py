2f3bf04a974d38cb199b5b0ca566ebe966cb7288
Richard Vu <rqvu.mail@gmail.com>
2023-03-26 01:04:22 -0700

    used argparse to parse arguments

--- serpy.py
+++ serpy.py
@@ -3,19 +3,19 @@ import sys, argparse
 
 ser = serial.Serial()
 ser.baudrate = 9600
-#ser.port = '/dev/cu.wchusbserial51850172361'
-ser.port = '/dev/cu.wchusbserial51851974134'
+ser.port = '/dev/cu.wchusbserial51850172361'
+#ser.port = '/dev/cu.wchusbserial51851974134'
 ser.open()
 print("Current port: " + ser.port)
 
 parser = argparse.ArgumentParser()
-parser.add_argument(color='color',)
+parser.add_argument(dest='color',)
+
+args = parser.parse_args()
+
 message = sys.argv[1].encode()
 
-if (message == 'm')
-
-
-print("Writing command: " + message.decode())
-ser.write(message)
+print(args)
+ser.write(messaargsge)
 
 ser.close()
