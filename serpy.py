1757383925c61d25e48758ac98f166875350f2e1
Richard Vu <rqvu.mail@gmail.com>
2023-03-27 01:20:48 -0700

    added random color option

--- serpy.py
+++ serpy.py
@@ -9,10 +9,14 @@ print("Current port: " + ser.port)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--color", help="b, g, r")
+parser.add_argument("--random", action="store_true", help="random color")
 
 args = parser.parse_args()
 
-print("Writing Message: " + args.color)
-ser.write(args.color.encode())
+if args.random:
+    print("random")
+else:
+    print("Writing Message: " + args.color)
+    ser.write(args.color.encode())
 
 ser.close()
