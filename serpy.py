698f0822cb65f63f65f5eb40584e3f88492aa477
Richard Vu <rqvu.mail@gmail.com>
2023-03-28 22:58:22 -0700

    added arg parsing for servo

--- serpy.py
+++ serpy.py
@@ -14,12 +14,18 @@ parser.add_argument("--random", action="store_true", help="random color")
 
 args = parser.parse_args()
 
+if args.color:
+    print("Writing Message: " + args.color)
+    ser.write(args.color.encode())
+
+if args.servo:
+    print("Servo angle: " + args.servo)
+    # ser.write(args.servo.encode())
+
 if args.random:
     print("random")
     while(True):
         ser.write('g'.encode())
-else:
-    print("Writing Message: " + args.color)
-    ser.write(args.color.encode())
+
 
 ser.close()
