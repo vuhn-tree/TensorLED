3b3eb38f6a5be71239ab64f878b433540f4e4ca2
Richard Vu <rqvu.mail@gmail.com>
2023-03-29 00:28:46 -0700

    pin defines

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,16 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire1);
 
+#define SERVOMIN 102  // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 512  // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN \
+  500  // This is the rounded 'minimum' microsecond length based on the minimum
+       // pulse of 102
+#define USMAX \
+  2500  // This is the rounded 'maximum' microsecond length based on the maximum
+        // pulse of 512
+#define SERVO_FREQ 50  // Analog servos run at ~50 Hz updates
+
 #define PORT_A 32
 #define PORT_B 26
 #define PORT_C 13
