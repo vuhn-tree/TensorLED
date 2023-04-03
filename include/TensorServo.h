4e1ade76edde82a7fda9c4bbc3a5a9da9894dcbd
Richard Vu <rqvu.mail@gmail.com>
2023-04-03 00:32:49 -0700

    moved servo defines to its dedicated class

--- include/TensorServo.h
+++ include/TensorServo.h
@@ -1,13 +1,20 @@
 #include <Wire.h>
-
 #include "Adafruit_PWMServoDriver.h"
 
-// Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire1);
+#define SERVOMIN 102  // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 512  // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN \
+  500  // This is the rounded 'minimum' microsecond length based on the minimum
+       // pulse of 102
+#define USMAX \
+  2500  // This is the rounded 'maximum' microsecond length based on the maximum
+        // pulse of 512
+#define SERVO_FREQ 50  // Analog servos run at ~50 Hz updates
+
 class TensorServo {
 public:
   TensorServo();
 
 private:
   Adafruit_PWMServoDriver m_pwm;
-//   Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire1);
 };
