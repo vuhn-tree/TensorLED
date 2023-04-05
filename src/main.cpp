7b3ce7bd5ad4faa6bad38c620c949564b269ab13
Richard Vu <rqvu.mail@gmail.com>
2023-04-04 21:34:49 -0700

    removed references to servo from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,5 @@
 #include <Adafruit_NeoPixel.h>
 #include <M5Core2.h>
-#include <Wire.h>
-
-#include "Adafruit_PWMServoDriver.h"
-
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire1);
-
-#define SERVOMIN 102  // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX 512  // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN \
-  500  // This is the rounded 'minimum' microsecond length based on the minimum
-       // pulse of 102
-#define USMAX \
-  2500  // This is the rounded 'maximum' microsecond length based on the maximum
-        // pulse of 512
-#define SERVO_FREQ 50  // Analog servos run at ~50 Hz updates
 
 #define PORT_A 32
 #define PORT_B 26
@@ -48,27 +33,6 @@ void setup() {
   M5.Lcd.drawString("RGB Monitor", 0, 0, 4);
 
   Serial.begin(9600);
-}
-
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-  pulselength = 1000000;  // 1,000,000 us per second
-  pulselength /= 50;      // 50 Hz
-  Serial.print(pulselength);
-  Serial.println(" us per period");
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength);
-  Serial.println(" us per bit");
-  pulse *= 1000;
-  pulse /= pulselength;
-  Serial.println(pulse);
-  pwm.setPWM(n, 0, pulse);
-}
-
-void servo_angle_write(uint8_t n, int Angle) {
-  double pulse = Angle;
-  pulse = pulse / 90 + 0.5;
-  setServoPulse(n, pulse);
 }
 
 void loop() {
@@ -130,7 +94,7 @@ void loop() {
   }
 
   // todo: servo
-  servo_angle_write(0, normalVal);
+  // servo_angle_write(0, normalVal);
 
   pixels.show();  // sends the updated color to the hardware.
   delay(500);
