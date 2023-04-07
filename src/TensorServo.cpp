05180da975887759d34a0da57cb712563cb5d5bd
Richard Vu <rqvu.mail@gmail.com>
2023-04-06 22:00:38 -0700

    format

--- src/TensorServo.cpp
+++ src/TensorServo.cpp
@@ -2,7 +2,7 @@
 #include <TensorServo.h>
 
 TensorServo::TensorServo() {
-Wire1.begin(21, 22);
+  Wire1.begin(21, 22);
   m_pwm.begin();
   m_pwm.setPWMFreq(50);
 }
@@ -11,7 +11,7 @@ void TensorServo::setServoPulse(uint8_t n, double pulse) {
   double pulselength;
   pulselength = 1000000;  // 1,000,000 us per second
   pulselength /= 50;      // 50 Hz
-  pulselength /= 4096;  // 12 bits of resolution
+  pulselength /= 4096;    // 12 bits of resolution
   pulse *= 1000;
   pulse /= pulselength;
   m_pwm.setPWM(n, 0, pulse);
