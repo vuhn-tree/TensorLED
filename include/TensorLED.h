4e2d3b03e3489cebf5264da4c2abca754fcb697d
Richard Vu <rqvu.mail@gmail.com>
2023-04-09 18:19:26 -0700

    move stuff to constructor

--- include/TensorLED.h
+++ include/TensorLED.h
@@ -12,14 +12,14 @@ Adafruit_NeoPixel m_pixels = Adafruit_NeoPixel(
     NUM_LEDS, PORT_B,
     NEO_GRB + NEO_KHZ800);  // set number of LEDs, pin number, LED type.
 
-const uint32_t RGB_RED = m_pixels.Color(100, 0, 0);
-const uint32_t RGB_GREEN = m_pixels.Color(0, 100, 0);
-const uint32_t RGB_BLUE = m_pixels.Color(0, 0, 100);
+// const uint32_t RGB_RED = m_pixels.Color(100, 0, 0);
+// const uint32_t RGB_GREEN = m_pixels.Color(0, 100, 0);
+// const uint32_t RGB_BLUE = m_pixels.Color(0, 0, 100);
 
-class TensorServo {
+class TensorLED {
  public:
-  TensorServo();
+  TensorLED();
   
  private:
-  
+  Adafruit_NeoPixel m_pixels;
 };
--- src/TensorLED.cpp
+++ src/TensorLED.cpp
@@ -1,1 +1,9 @@
 #include <TensorLED.h>
+
+  TensorLED::TensorLED() {
+    m_pixels = Adafruit_NeoPixel(
+    NUM_LEDS, PORT_B,
+    NEO_GRB + NEO_KHZ800);  // set number of LEDs, pin number, LED type.
+    m_pixels.begin();
+    m_pixels.setBrightness(10);
+  }
