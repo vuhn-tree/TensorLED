754ecae36b3ae282c3eb2569d7c48c5a364ec0c7
Richard Vu <rqvu.mail@gmail.com>
2023-04-09 23:29:10 -0700

    removed unused

--- include/TensorLED.h
+++ include/TensorLED.h
@@ -6,12 +6,6 @@
 
 #define NUM_LEDS 3
 
-const int DISP_OFFSET = 25;
-
-Adafruit_NeoPixel m_pixels = Adafruit_NeoPixel(
-    NUM_LEDS, PORT_B,
-    NEO_GRB + NEO_KHZ800);  // set number of LEDs, pin number, LED type.
-
 // const uint32_t RGB_RED = m_pixels.Color(100, 0, 0);
 // const uint32_t RGB_GREEN = m_pixels.Color(0, 100, 0);
 // const uint32_t RGB_BLUE = m_pixels.Color(0, 0, 100);
