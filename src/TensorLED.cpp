ee0ddef65b9a418a83a9fa1104f76f6690444043
Richard Vu <rqvu.mail@gmail.com>
2023-04-15 16:35:46 -0700

    more cleanup

--- src/TensorLED.cpp
+++ src/TensorLED.cpp
@@ -1,19 +1,8 @@
 #include <TensorLED.h>
 
-// uint32_t RGB_RED;
-// uint32_t RGB_GREEN;
-// uint32_t RGB_BLUE;
-
 TensorLED::TensorLED() {
-//   m_pixels = Adafruit_NeoPixel(
-//       NUM_LEDS, PORT_B,
-//       NEO_GRB + NEO_KHZ800);  // set number of LEDs, pin number, LED type.
   m_pixels.begin();
   m_pixels.setBrightness(10);
-
-//   RGB_RED = m_pixels.Color(100, 0, 0);
-//   RGB_GREEN = m_pixels.Color(0, 100, 0);
-//   RGB_BLUE = m_pixels.Color(0, 0, 100);
 }
 
 void TensorLED::setLEDColor(char colorInput) {
