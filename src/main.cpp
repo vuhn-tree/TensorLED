9734c60bd33be2c9491f576e4adb1b211d1c1841
Richard Vu <rqvu.mail@gmail.com>
2023-12-30 00:10:30 -0800

    put in servo code

--- src/main.cpp
+++ src/main.cpp
@@ -53,10 +53,10 @@ void loop() {
   // sprintf(buf, "vin: %2.2fV %2.1fmA", vinV, vinA);
   // M5.Lcd.drawString(buf, 0, DISP_OFFSET * 4, 4);
 
-  // potVal = analogRead(PORT_C);  // read the value from the sensor.
-  // const int normalVal = map(potVal, 0, 4096, 0, 255);
-  // sprintf(buf, "Norm Pot: %03d%", normalVal);
-  // M5.Lcd.drawString(buf, 0, DISP_OFFSET * 5, 4);
+  potVal = analogRead(PORT_C);  // read the value from the sensor.
+  const int normalVal = map(potVal, 0, 4096, 0, 255);
+  sprintf(buf, "Norm Pot: %03d%", normalVal);
+  M5.Lcd.drawString(buf, 0, DISP_OFFSET * 5, 4);
 
   // const int lcdPotVal = map(potVal, 0, 4096, 2500, 3300);
   // M5.Axp.SetLcdVoltage(lcdPotVal);
@@ -71,7 +71,8 @@ void loop() {
 
 
   tensorLED->setColor(colorInput);
-  // tensorServo->servo_angle_write(0, normalVal);
+  tensorServo->servo_angle_write(0, normalVal);
+  tensorServo->servo_angle_write(1, normalVal);
 
   delay(50);
 
